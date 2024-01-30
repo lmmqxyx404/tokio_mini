65ae2a441776f875f3863b0e3d422ec390484f77
lmmqxyx <1368470804@qq.com>
2024-01-30 19:20:57 +0800

    add another struct UnsafeCell the same as AtomicUsize

--- tokio/src/loom/std/mod.rs
+++ tokio/src/loom/std/mod.rs
@@ -1,7 +1,14 @@
 mod atomic_usize;
+mod unsafe_cell;
 
+/// 1.std::sync
 pub(crate) mod sync {
     pub(crate) mod atomic {
         pub(crate) use crate::loom::std::atomic_usize::AtomicUsize;
     }
+}
+
+/// 2.std::cell
+pub(crate) mod cell {
+    pub(crate) use super::unsafe_cell::UnsafeCell;
 }
